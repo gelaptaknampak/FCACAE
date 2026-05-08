c8944fc1e1eb20eba7dfc7b2629f8f2df82c90b8
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2026-05-09 01:07:50 +0700

    Update mainFCAC.py

--- mainFCAC.py
+++ mainFCAC.py
@@ -7,10 +7,8 @@ import networkx as nx
 import numpy as np
 from tqdm.auto import tqdm
 from utility_fl import *
-import sys
 
 from fcac import FCAC
-import traceback
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -50,66 +48,54 @@ all_ari = []
 all_ami = []
 all_nmi = []
 
-try:
-    print(data_name)
-    for i_trial in tqdm(range(n_trial), total=n_trial, desc='Trial for Averaging'):  # for averaging
 
-        # load dataset
-        DATA, TARGET, n_clients, n_classes = set_dataset(data_name, niid, i_trial)
+print(data_name)
+for i_trial in tqdm(range(n_trial), total=n_trial, desc='Trial for Averaging'):  # for averaging
 
-        DATA = DATA.astype(np.float32)
+    # load dataset
+    DATA, TARGET, n_clients, n_classes = set_dataset(data_name, niid, i_trial)
 
-        # training data = test data
-        train_DATA = DATA
-        train_TARGET = TARGET
-        test_data = DATA
-        test_target = TARGET
-        test_dataset = {"full_data": test_data, "true_label": test_target}
+    # training data = test data
+    train_DATA = DATA
+    train_TARGET = TARGET
+    test_data = DATA
+    test_target = TARGET
+    test_dataset = {"full_data": test_data, "true_label": test_target}
 
-        # prepare for federated learning
-        train_data, train_target, statistic = separate_data((train_DATA, train_TARGET), n_clients, n_classes, alpha, niid, balance, partition)
+    # prepare for federated learning
+    train_data, train_target, statistic = separate_data((train_DATA, train_TARGET), n_clients, n_classes, alpha, niid, balance, partition)
 
-        # Add Laplacian noise to a train_dataset
-        if epsilon == -1:  # no noise setting
-            noised_train_data = train_data
-        else:
-            noised_train_data = [add_laplace_noise(data, epsilon, seed=i_trial) for data in train_data]
+    # Add Laplacian noise to a train_dataset
+    if epsilon == -1:  # no noise setting
+        noised_train_data = train_data
+    else:
+        noised_train_data = [add_laplace_noise(data, epsilon, seed=i_trial) for data in train_data]
 
-        noised_train_data = [
-            client_data.astype(np.float32) 
-            for client_data in noised_train_data
-        ]
+    # training
+    fcac = FCAC(n_clients_=n_clients, iter_server_=max_iters)
+    start = time.time()
+    params_server_fcac, params_clients_fcac = fcac.fit(noised_train_data)
+    all_training_time.append(time.time() - start)
 
-        # training
-        fcac = FCAC(n_clients_=n_clients, iter_server_=max_iters)
-        start = time.time()
-        params_server_fcac, params_clients_fcac = fcac.fit(noised_train_data)
-        all_training_time.append(time.time() - start)
+    # test
+    server_assignments = params_server_fcac.predict(test_data)
 
-        # test
-        server_assignments = params_server_fcac.predict(test_data)
-
-        # evaluation
-        all_ari.append(adjusted_rand_score(test_dataset['true_label'], server_assignments))
-        all_ami.append(adjusted_mutual_info_score(test_dataset['true_label'], server_assignments))
-        all_nmi.append(normalized_mutual_info_score(test_dataset['true_label'], server_assignments))
-        all_n_nodes.append(params_server_fcac.G_.number_of_nodes())
-        all_n_clusters.append(params_server_fcac.n_clusters_)
-        # Menghitung total ukuran memori (dalam bytes) dari seluruh data klien
-        total_payload = sum(client_data.nbytes for client_data in noised_train_data)
+    # evaluation
+    all_ari.append(adjusted_rand_score(test_dataset['true_label'], server_assignments))
+    all_ami.append(adjusted_mutual_info_score(test_dataset['true_label'], server_assignments))
+    all_nmi.append(normalized_mutual_info_score(test_dataset['true_label'], server_assignments))
+    all_n_nodes.append(params_server_fcac.G_.number_of_nodes())
+    all_n_clusters.append(params_server_fcac.n_clusters_)
+    # Menghitung total ukuran memori (dalam bytes) dari seluruh data klien
+    total_payload = sum(client_data.nbytes for client_data in noised_train_data)
 
 
-    # averaged results
-    print(data_name)
-    print('--------------- FCAC (mean result)')
-    print('Time:', '{:.5f}'.format(np.mean(all_training_time)), '[s]')
-    print(f"Payload Komunikasi Asli: {total_payload} bytes")
-    print(' # of Nodes:', '{:.1f}'.format(np.mean(all_n_nodes)))
-    print(' # of Clusters:', '{:.1f}'.format(np.mean(all_n_clusters)))
-    print(' ARI:', '{:.5f}'.format(np.mean(all_ari)))
-    print(' AMI:', '{:.5f}'.format(np.mean(all_ami)))
-    print(' NMI:', '{:.5f}'.format(np.mean(all_nmi)))
-
-except Exception as e:
-    print("ERROR TERJADI:")
-    traceback.print_exc()
+# averaged results
+print(data_name)
+print('--------------- FCAC (mean result)')
+print('Time:', '{:.5f}'.format(np.mean(all_training_time)), '[s]')
+print(' # of Nodes:', '{:.1f}'.format(np.mean(all_n_nodes)))
+print(' # of Clusters:', '{:.1f}'.format(np.mean(all_n_clusters)))
+print(' ARI:', '{:.5f}'.format(np.mean(all_ari)))
+print(' AMI:', '{:.5f}'.format(np.mean(all_ami)))
+print(' NMI:', '{:.5f}'.format(np.mean(all_nmi)))
