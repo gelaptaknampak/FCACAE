9cdac990f8b0f98ad3a7194e04112cb0ec7ec05a
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2026-06-02 14:51:33 +0700

    n trial

--- mainFCACFAE.py
+++ mainFCACFAE.py
@@ -31,7 +31,7 @@ data_name = "fmnist"
 
 
 # experimental settings
-n_trial = 10
+n_trial = 2
 niid = True  # True:non-iid, False:iid for federated learning
 epsilon = -1  # privacy budget for \epsilon-differential privacy (-1: no noise)
 max_iters = 1
